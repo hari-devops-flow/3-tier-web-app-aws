c281bb069b7806e22dd7f096972ec8258de1916f
Harishankar Dubey <hari@example.com>
2024-12-25 21:30:38 +0530

    public subnet 2 added

--- variables.tf
+++ variables.tf
@@ -9,3 +9,9 @@ variable "public_subnet_cidr_1" {
   type        = string
   default     = "10.0.1.0/24"
 }
+
+variable "public_subnet_cidr_2" {
+  description = "Map for public subnets CIDR in different AZ."
+  type        = string
+  default     = "10.0.2.0/24"
+}
--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -7,11 +7,22 @@ resource "aws_vpc" "sandbox_vpc" {
   }
 }
 
-resource "aws_subnet" "web" {
-  cidr_block = var.public_subnet_cidr_1
-  vpc_id     = aws_vpc.sandbox_vpc.id
+resource "aws_subnet" "web_1" {
+  cidr_block        = var.public_subnet_cidr_1
+  vpc_id            = aws_vpc.sandbox_vpc.id
+  availability_zone = "us-east-1a"
   tags = {
     Name = "web_subnet_1"
+    Type = "public"
+  }
+}
+
+resource "aws_subnet" "web_2" {
+  cidr_block        = var.public_subnet_cidr_2
+  vpc_id            = aws_vpc.sandbox_vpc.id
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "web_subnet_2"
     Type = "public"
   }
 }
