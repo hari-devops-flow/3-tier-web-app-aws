c119d7e2b9ee8d35b4eef139bf22eaffe3c6520d
Harishankar Dubey <hari@example.com>
2024-12-25 21:42:32 +0530

    az_id added

--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -8,9 +8,9 @@ resource "aws_vpc" "sandbox_vpc" {
 }
 
 resource "aws_subnet" "web_1" {
-  cidr_block        = var.public_subnet_cidr_1
-  vpc_id            = aws_vpc.sandbox_vpc.id
-  availability_zone = "us-east-1a"
+  cidr_block           = var.public_subnet_cidr_1
+  vpc_id               = aws_vpc.sandbox_vpc.id
+  availability_zone_id = "use1-az1"
   tags = {
     Name = "web_subnet_1"
     Type = "public"
@@ -18,9 +18,9 @@ resource "aws_subnet" "web_1" {
 }
 
 resource "aws_subnet" "web_2" {
-  cidr_block        = var.public_subnet_cidr_2
-  vpc_id            = aws_vpc.sandbox_vpc.id
-  availability_zone = "us-east-1b"
+  cidr_block           = var.public_subnet_cidr_2
+  vpc_id               = aws_vpc.sandbox_vpc.id
+  availability_zone_id = "use1-az2"
   tags = {
     Name = "web_subnet_2"
     Type = "public"
