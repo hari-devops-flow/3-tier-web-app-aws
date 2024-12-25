603b6e8dcf25a313ca06e6413b1cb9621cdf2da8
Harishankar Dubey <hari@example.com>
2024-12-25 21:20:41 +0530

    vpc public_subnet created

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
   type        = string
 }
+
+variable "public_subnet_cidr_1" {
+  description = "Map for public subnets CIDR in different AZ."
+  type        = string
+  default     = "10.0.1.0/24"
+}
--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "sandbox_vpc" {
 }
 
 resource "aws_subnet" "web" {
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.public_subnet_cidr_1
   vpc_id     = aws_vpc.sandbox_vpc.id
   tags = {
     Name = "web_subnet_1"
